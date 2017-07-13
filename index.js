1bd3c3fec2c950e84213eed3ad1297e918c3ddd2
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2017-07-12 23:10:06 -0700

    upload for WeAllJs

--- index.js
+++ index.js
@@ -1,68 +1,85 @@
+const {shell} = require('electron')
+
 $(document).ready(function(){
+    $('.text').load('start.html');
     var docs=[['jay.html','whoAmI'],
       ['electron.html', 'electron'],
-      ['hard.html', 'hard'],
       ['install.html', 'install'],
+      ['challenge.html', 'challenge'],
       ['gotchas.html', 'gotchas']]
 
-    var docIndex = 0;
+    var docIndex = -1;
 
-    function selectID(page, id) {
-        $(".text").load(page);
-        $(".text").attr('id', id)
-        docIndex = docs.indexOf(page);
+    function incrementDocs() {
+
+    if (docIndex < docs.length - 1){
+      docIndex ++;
+        }
+
+    else {
+     docIndex = 0;
+       };
+       return this;
+     };
+
+     function decrementDocs() {
+       if (docIndex <= 0){
+         docIndex = docs.length - 1;
+          }
+
+       else {
+        docIndex --;
+          };
+
+        return this;
+     };
+
+    function selectID(doc) {
+        $(".text").load(doc[0]);
+        $(".text").attr('id', doc[1]);
+        docIndex = docs.indexOf(doc);
         return this;
       };
 
   $("#whoAmI").click(function(){
-    selectID('jay.html', 'whoAmI');
+    selectID(docs[0]);
   });
   $("#whatIsElectron").click(function(){
-      selectID('electron.html', 'whatIsElectron');
-  });
-
-  $("#hard").click(function(){
-    selectID('hard.html', 'hard');
+      selectID(docs[1]);
   });
 
   $("#install").click(function(){
-    selectID('install.html', 'install');
+    selectID(docs[2]);
+  });
+  $("#challenge").click(function(){
+    selectID(docs[3]);
   });
 
   $("#gotchas").click(function(){
-    selectID('gotchas.html', 'gotchas');
+    selectID(docs[4]);
   });
 
   $(document).keydown(function(e) {
-    var doc = docs[docIndex];
-    if (e.keyCode == 39) { //Right
-      selectID(doc[0], doc[1])
 
-     if (docIndex < docs.length - 1){
-       docIndex ++;
-        }
-
-     else {
-      docIndex = 0;
-        };
+    if (e.keyCode == 39 || e.keyCode == 13) { //Right or Enter
+      incrementDocs();
+      console.log('Doc Length is ' + docs.length);
+      console.log('Doc Index is ' + docIndex);
+      selectID(docs[docIndex]);
 
     };
 
     if (e.keyCode == 37) { //Left
-      var doc = docs[docIndex];
-      selectID(doc[0], doc[1])
-
-     if (docIndex == 0){
-       docIndex = docs.length - 1;
-        }
-        
-     else {
-      docIndex --;
-        };
+      decrementDocs();
+      selectID(docs[docIndex]);
 
       };
     if (e.keyCode == 27) { // esc
-      $(".text").text('')
+      $('.text').load('start.html');
+      docIndex=-1
       }
+    });
+    $('.text').on('click', '.challenge li', function () {
+      $( this ).toggleClass('strike');
     });
 });
